18f27b85d575f80fa2bd85599b09fec04e83c6a6
jamesdeal89 <j891319@protonmail.com>
2022-02-10 15:06:26 +0800

    Added machine learning notes

--- faceDetection.py
+++ faceDetection.py
@@ -1,13 +1,24 @@
 # a basic face detector from OpenAI - made following an AI course
 
 """
-notes on the haar cascade method for facial detection:
-the haar method uses simple differences in whites and blacks to determine what a face should look like.
-This is why it only needs a greyscaled image as it doesn't detect facial features, 
-just typical gradient changes and locations in a typical face.
+Notes on the haar cascade method of Machine Learning:
+The haar method uses simple differences in whites and blacks to determine what an object should look like. 
+This is why it only needs a greyscaled image.
+just typical gradient changes and locations using several haar blocks for edges, corners, etc.
 This would mean, for example, the line of darker colour around the eyeline and eyebrows horizontally, the vertical
-gradient of light to dark along the nose, and other defining features. 
-for example in this image: https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fmiro.medium.com%2Fmax%2F1400%2F1%2Agl4JHntNPHQt1G7txpiRMA.png&sp=1644455905T0894db642a08598a603fe1f2f3c45ff055c88dfab0d26c19a4442481cddeb8db
+gradient of light to dark along the nose, and other defining features if we were training for faces.
+For example in this image: https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fmiro.medium.com%2Fmax%2F1400%2F1%2Agl4JHntNPHQt1G7txpiRMA.png&sp=1644455905T0894db642a08598a603fe1f2f3c45ff055c88dfab0d26c19a4442481cddeb8db
+
+We can create our own training data with supervised machine learning.
+This means feeding the algorithm 'positive' and 'negative' data.
+In this use-case, positive data would be an image of a face and negative data would be an image of anything other than a face.
+Alternatively there is un-supervised machine learning which means un-labeled training data is given to the algorithm and 
+it just has to guess whilst the programmer marks it's responses as 'correct' or 'incorrect'.
+The machine learns based on that feedback. 
+
+In the code bellow, I used an OpenCV provided .xml file which contains pre-trained data.
+The image data is run through this and it checks for each haar cascade the .xml file has listed 
+and checks that it passes the provided gradient locations and levels.
 """
 
 import cv2
