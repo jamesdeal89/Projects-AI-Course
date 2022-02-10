eb9e8a4788397d255c982cfd75e670ca1a20c7a3
jamesdeal89 <j891319@protonmail.com>
2022-02-10 09:22:37 +0800

    Added some commented notes on Haar Cascade

--- faceDetection.py
+++ faceDetection.py
@@ -1,5 +1,15 @@
 # a basic face detector from OpenAI - made following an AI course
 
+"""
+notes on the haar cascade method for facial detection:
+the haar method uses simple differences in whites and blacks to determine what a face should look like.
+This is why it only needs a greyscaled image as it doesn't detect facial features, 
+just typical gradient changes and locations in a typical face.
+This would mean, for example, the line of darker colour around the eyeline and eyebrows horizontally, the vertical
+gradient of light to dark along the nose, and other defining features. 
+for example in this image: https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fmiro.medium.com%2Fmax%2F1400%2F1%2Agl4JHntNPHQt1G7txpiRMA.png&sp=1644455905T0894db642a08598a603fe1f2f3c45ff055c88dfab0d26c19a4442481cddeb8db
+"""
+
 import cv2
 
 # trained data from OpenAI github
@@ -33,7 +43,7 @@ while True:
         cv2.rectangle(img=frame, pt1=(x,y), pt2=(x+w,y+h), color=(0,255,0), thickness=2)
 
     # creates a UI windows and displays the image
-    cv2.imshow(winname="face detection", mat=frame)
+    cv2.imshow(winname="faces", mat=frame)
 
 
 # ends processes
